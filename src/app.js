e3a0c62f9628fb49012888cadf0f5b3d14c22662
roop <roop@roop.ie>
2019-05-30 10:25:15 +0100

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,112 @@
+
+const path = require('path')//core modules on top
+const express =require('express')
+const hbs = require('hbs')
+const geocode  = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+const app = express()
+
+//customise server ,define paths for EXPRESS CONFIG-
+const mypath =path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../templates/views')
+const partialsPath = path.join(__dirname,'../templates/partials')
+
+//setup handlebars engine and views location
+app.set('view engine','hbs')
+app.set('views',viewsPath)//tell express to use views directory
+hbs.registerPartials(partialsPath)
+//create a partial
+
+
+//setup static directory to serve
+app.use(express.static(mypath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title:'VaayuVaruna- The weatherApp',
+        name:'VaayuVaruna'
+    })
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+       title:'About us',
+       name:'VaayuVaruna'
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        title:'Help',
+        email:'help@vaayuvaruna.ie'
+    })
+})
+app.get('/weather',(req,res)=>{
+    // if there is no address send back an error message
+   if(!req.query.address){
+       return res.send({
+        error:' please include an address to search'
+    })
+    }
+    //callback chaining
+    geocode(req.query.address,(error, {latitude,longitude,location})=>{
+        if(error){
+            return res.send({error})
+        }
+
+        forecast(latitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+        
+            res.send({
+            forecast:forecastData,
+            location,
+            address:req.query.address
+
+            })
+
+        })
+    })
+})
+
+   
+    
+//query string example
+// app.get('/products',(req,res)=>{
+    //ensure there is an search term in the query
+//     if(!req.query.search){
+//          return res.send({
+//             error:'You must provide a search term'
+//         })
+//     }
+//     console.log(req.query.search)
+//     res.send({
+//         products:[]
+//     })
+// })
+
+app.get('/help/*',(req,res)=>{
+    res.render('notFound',{
+        title: '404',
+        errorMessage:'Sorry Article not found'
+    })
+
+})
+
+//using a wildcard * character to 
+//match anything that is not specified so far in above routes 
+//for the app.get function
+app.get('*',(req,res)=>{
+    res.render('notFound',{
+        title:'404',
+        errorMessage:'Sorry, page not found'
+    })
+})
+
+
+//start the server
+app.listen(3000,()=>{
+console.log('server is up on port 3000')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+const geocode  = require('./geocode')
+
+const forecast = (latitude,longitude,callback)=>{
+    const url = 'https://api.darksky.net/forecast/98e3cc4882db5227e5164153c9a75147/' + latitude +',' + longitude
+   // console.log('forcast url : ', url);
+    request({url, json: true}, (error,{body}) => {
+        if(error){
+            callback('unable to connect to weather service',null)
+        }else if(body.error){
+            callback('Unable to fetch location', null)
+        }else{
+           // console.log('Body : ', body);
+            callback(null, body.daily.data[0].summary+'It is currently ' + body.currently.temperature + ' degrees out. There is '+ body.currently.precipProbability + '% chances of rain')
+            }
+    })
+
+
+}
+module.exports = forecast
